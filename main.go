cb6d3e0b1e045327df9ea26fa5116eef32f2ade3
lyespinosa <leonardoyahelespinosa@gmail.com>
2023-10-31 17:55:10 -0600

    arreglar los elementos perdidos en la cola de espera que nunca se guardan

--- main.go
+++ main.go
@@ -9,30 +9,48 @@ import (
 
 func productor(buffer chan<- int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for i := 0; i < 30; i++ {
-		fmt.Println("+ Prodocido: ", i)
+	for i := 0; i < 100; i++ {
+		fmt.Println("Producido:", i)
 		buffer <- i
-		time.Sleep(time.Second)
+		time.Sleep(time.Millisecond * 100)
 	}
 	close(buffer)
 }
 
-func almacenador(buffer <-chan int, wg *sync.WaitGroup) {
+func elementoVive(num int, almacen *[]int, espacioDisponible chan<- bool, mu *sync.Mutex, wg *sync.WaitGroup) {
 	defer wg.Done()
+	tiempo := time.Duration(rand.Intn(3)+1) * time.Second
+	time.Sleep(tiempo)
 
-	almacen := make([]int, 0, 20)
-
-	for num := range buffer {
-		if len(almacen) < 20 {
-			almacen = append(almacen, num)
-			fmt.Println("Almacenado", almacen)
-		} else {
-			index := rand.Intn(20)
-			fmt.Printf("Sacando: %d, del lugar: %d\n", almacen[index], index)
-			almacen[index] = num
-			fmt.Println("Almacen actualizado", almacen)
+	mu.Lock()
+	// Elimina el elemento después de que ha vivido su tiempo aleatorio.
+	for i, val := range *almacen {
+		if val == num {
+			*almacen = append((*almacen)[:i], (*almacen)[i+1:]...)
+			fmt.Printf("Elemento eliminado: %d. Almacen actualizado: %v, ocupados %v \n", num, *almacen, len(*almacen)) // Editado
+			if len(*almacen) < cap(*almacen) {
+				// Si hay espacio disponible, envía una señal al canal.
+				espacioDisponible <- true // Editado
+			}
+			break
 		}
-		time.Sleep(2 * time.Second)
+	}
+	mu.Unlock()
+}
+
+func almacenador(buffer <-chan int, almacen *[]int, espacioDisponible chan<- bool, mu *sync.Mutex, wg *sync.WaitGroup) {
+	for num := range buffer {
+		mu.Lock()
+		if len(*almacen) < cap(*almacen) {
+			*almacen = append(*almacen, num)
+			fmt.Println("Almacenado el ", num, ": ", *almacen, "Ocupados:", len(*almacen))
+			wg.Add(1)
+			go elementoVive(num, almacen, espacioDisponible, mu, wg)
+		} else {
+			// Si el almacen está lleno, envía una señal al canal.
+			espacioDisponible <- true // Editado
+		}
+		mu.Unlock()
 	}
 }
 
@@ -40,13 +58,31 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	var wg sync.WaitGroup
-	buffer := make(chan int, 5)
+	mu := &sync.Mutex{}
+	buffer := make(chan int, 5)          // Canal para los elementos producidos.
+	almacen := make([]int, 0, 20)        // Almacen con capacidad para 20 elementos.
+	espacioDisponible := make(chan bool) // Canal para señalizar cuando hay espacio disponible.
 
 	wg.Add(1)
 	go productor(buffer, &wg)
 
-	wg.Add(1)
-	go almacenador(buffer, &wg)
+	// Inicia el almacenador en su propia goroutine.
+	go almacenador(buffer, &almacen, espacioDisponible, mu, &wg)
 
-	wg.Wait()
+	// Gestiona el espacio disponible en el almacen.
+	go func() {
+		for range espacioDisponible {
+			mu.Lock()
+			if len(almacen) < cap(almacen) && len(buffer) > 0 {
+				num := <-buffer // Toma el siguiente elemento en espera.
+				almacen = append(almacen, num)
+				fmt.Println("Almacenado desde espera:", &almacen)
+				wg.Add(1)
+				go elementoVive(num, &almacen, espacioDisponible, mu, &wg)
+			}
+			mu.Unlock()
+		}
+	}()
+
+	wg.Wait() // Espera a que todos los elementos hayan sido procesados.
 }
